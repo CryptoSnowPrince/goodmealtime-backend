1be0c06bb4792a2d64cf3ad9c9d383e8d3e1b342
CryptoSnowPrince <topdirector2017@gmail.com>
2023-01-16 05:34:25 -0500

    update

--- test.php
+++ test.php
@@ -5,8 +5,8 @@
     }
 
     $servername = "localhost";
-    $username = "root";
-    $password = "";
+    $username = "dev_Alek";
+    $password = "_6X;$7,Cl!}G";
     $dbname = "i7452067_wp3";
 
     // Create connection
@@ -16,6 +16,17 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+	
+	function utf8ize($d) {
+	    if (is_array($d)) {
+	        foreach ($d as $k => $v) {
+	            $d[$k] = utf8ize($v);
+	        }
+	    } else if (is_string ($d)) {
+	        return utf8_encode($d);
+	    }
+	    return $d;
+	}
 
     function getMeals(){
         global $conn;
@@ -77,8 +88,8 @@
             } 
 
             $response_final = array($response_data);
-
-            echo json_encode($response_final);
+			
+            echo json_encode(utf8ize($response_final));
 
         } else{
             $sql = "select t3.*, t4.guid as image from
@@ -166,7 +177,7 @@
 
             }
 
-            echo json_encode($new_item);
+            echo json_encode(utf8ize($new_item));
         }
     }
 
@@ -184,8 +195,7 @@
             // order by t1.id desc
             // ";
 
-            $sql = "select t5.*, t6.comment_content as description from
-            (select t3.*, t4.display_name as name from 
+            $sql = "select t3.*, t4.display_name as name from 
             (select t1.*,t2.* from
             (select event_id as id, event_name as title, post_id, post_content, event_owner, event_status, event_start_date as date, event_start_time as time from wp_em_events) as t1
             left join
@@ -193,10 +203,7 @@
             on t1.id = t2.event_id) as t3
             left join
             wp_users t4
-            on t3.event_owner = t4.ID) as t5
-            left join
-            wp_comments t6
-            on t5.post_id = t6.comment_post_ID";
+            on t3.event_owner = t4.ID";
 
             $result = $conn->query($sql);
 
@@ -231,11 +238,10 @@
                     array_push($response_data, $new_item);
                 }
             }
-            echo json_encode($response_data);
+            echo json_encode(utf8ize($response_data));
         }else{
 
-            $sql = "select t5.*, t6.comment_content as description from
-            (select t3.*, t4.display_name as eventManager from 
+            $sql = "select t3.*, t4.display_name as eventManager from 
             (select t1.*,t2.* from
             (select event_id as id, event_name as name, post_id, post_content, event_owner, event_status, event_start_date as date, event_start_time as time from wp_em_events where event_id = ".$event_id.") as t1
             left join
@@ -243,10 +249,7 @@
             on t1.id = t2.event_id) as t3
             left join
             wp_users t4
-            on t3.event_owner = t4.ID) as t5
-            left join
-            wp_comments t6
-            on t5.post_id = t6.comment_post_ID
+            on t3.event_owner = t4.ID
             ";
 
             $result = $conn->query($sql);
@@ -293,7 +296,7 @@
                     
                 }
             }
-            echo json_encode($new_item);
+            echo json_encode(utf8ize($new_item));
 
         }
     }
@@ -339,7 +342,7 @@
                     
                 }
             }
-            echo json_encode($new_item);
+            echo json_encode(utf8ize($new_item));
         }
     }
 
